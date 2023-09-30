a47955101e3e77d0b6a380e5f4243179309cbbfe
elhamrifouad <fouad.elhamri@bits.com.kw>
2023-09-30 10:04:43 +0100

    create auth routing

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      {children}
+    </main>
+  );
+};
+
+export default AuthLayout;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex-center h-full flex-col">
-      <h1 className="h1-bold">NExt js is coming suuuupeeer </h1>
-      <h2 className="h2-bold">NExt js is coming suuuupeeer </h2>
-      <h3 className="h3-bold">NExt js is coming suuuupeeer </h3>
-    </div>
-  );
-}
