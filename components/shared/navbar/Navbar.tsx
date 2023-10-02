4aec735b9d40d738e20734c7951db0683384b303
elhamrifouad <fouad.elhamri@bits.com.kw>
2023-10-02 09:35:55 +0100

    create navbar skeleton

---
+++ components/shared/navbar/Navbar.tsx
@@ -0,0 +1,41 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          alt="askCom-logo"
+          width={23}
+          height={23}
+          src="/assets/images/site-logo.svg"
+        />
+        <p className="h2-bold font-spaceGrotesk text-dark-500 dark:text-light-900 max-sm:hidden">
+          Ask <span className="text-primary-500">Community</span>
+        </p>
+      </Link>
+      GLobal Search
+      <div className="flex-between gap-5 ">
+        Theme
+        <SignedIn>
+          <UserButton
+            afterSignOutUrl="/"
+            appearance={{
+              elements: {
+                avatarBox: "h-10 w-10",
+              },
+              variables: {
+                colorPrimary: "#ff7000",
+              },
+            }}
+          />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
