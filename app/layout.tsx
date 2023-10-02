b4b8062157098e8f3ae5a9ad80cf5e2549b0d0ce
elhamrifouad <fouad.elhamri@bits.com.kw>
2023-10-02 08:01:26 +0100

    add custom theme to clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,28 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export const metadata: Metadata = {
   title: "Ask Community",
-  description: "A platform where developer can get answer to their bugs 🐛",
+  description:
+    "A community-driven platform for asking and answering programming questions \n Get help, share knowledge, and collaborate with developers from around the world. \n Explore topics in web development, mobile dev, algorithms, data structures, and more...",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -17,9 +31,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className + " " + spaceGrotesk.className}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
